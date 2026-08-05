521319f228521a61080210ee18f1230cfda80dae
Victor Ramirez <Vicramirez_10@hotmail.com>
2026-08-05 11:23:22 -0600

    Update habits.service.ts

--- src/habits/habits.service.ts
+++ src/habits/habits.service.ts
@@ -58,7 +58,7 @@ export class HabitsService {
   create(createHabitDto: CreateHabitDto) {
     return habits.push({
       id: habits.length + 1,
-      ...createHabitDto, // destructuring the properties from createHabitDto
+      ...createHabitDto, // destructuring the properties from createHabitDto test
       startedAt: new Date(),
       lastUpdatedAt: new Date(),
     });
